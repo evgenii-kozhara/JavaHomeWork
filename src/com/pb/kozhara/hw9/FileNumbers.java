334bed473ea79972a5be993dbfca8579608725fd
Evgenii Kozhara <evgeniikozhara@gmail.com>
2021-11-28 16:35:23 +0200

    init commit

--- src/com/pb/kozhara/hw9/FileNumbers.java
+++ src/com/pb/kozhara/hw9/FileNumbers.java
@@ -1,6 +1,8 @@
 package com.pb.kozhara.hw9;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,7 +10,7 @@ import java.util.Random;
 
 public class FileNumbers {
     public static void createNumbersFile() throws IOException {
-        Path path = Files.createFile(Paths.get("C:\\Users\\dn270298kej\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers.txt\"));
+        Path path = Files.createFile(Paths.get("C:\\Users\\dn270298kej\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers\"));
     Random r = new Random();
     int[] arr = new int[100];
     for (int i = 0; i < arr.length; i++)
@@ -23,8 +25,8 @@ public class FileNumbers {
     }
     }
     public static void createOddNumbersFile() throws IOException {
-        Path path = Paths.get("C:\\Users\\dn270298kej\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers.txt\"))
-    try (BufferedWriter w = Files.newBufferedWriter(anotherPath)) {
+        Path path = Paths.get("C:\\Users\\dn270298kej\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers\"))
+    try (BufferedWriter w = Files.newBufferedWriter(annotherPath)) {
         for (int n = 0; n <= array.length - 1; n++) {
             if (n % 10 == 0) {
                 w.write("\n" + array[n] + " ");
