13fe817511b1c9b733aca8cb640f0784d1d8d9cf
Evgenii Kozhara <evgeniikozhara@gmail.com>
2021-12-01 18:31:18 +0200

    init commit

--- src/com/pb/kozhara/hw9/FileNumbers.java
+++ src/com/pb/kozhara/hw9/FileNumbers.java
@@ -9,41 +9,61 @@ import java.nio.file.Paths;
 import java.util.Random;
 
 public class FileNumbers {
-    public static void createNumbersFile() throws IOException {
-        Path path = Files.createFile(Paths.get("C:\\Users\\dn270298kej\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers\"));
-    Random r = new Random();
-    int[] arr = new int[100];
-    for (int i = 0; i < arr.length; i++)
-        arr[i] = r.nextInt(100);
-    try (BufferedWriter w = Files.newBufferedWriter(path)) {
-        for (int i = 0; i <= arr.length - 1; i++) {
-            if (i % 10 == 0 && i != 100)
-                w.write("\n" + arr[i] + " ");
-            else
-                w.write(arr[i] + " ");
+    public static void main(String[] args) throws Exception {
+        int size = 100;
+        String nrFilePath = "Windows-SSD(C):\\Users\\38099\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers";
+        String oddPath = "Windows-SSD(C):\\Users\\38099\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers";
+        int[] array = new int[size]; // вихідний масив
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt((size-1)) + 1;
         }
+        createNumbersFile(array, nrFilePath);
+        createOddNumbersFile(nrFilePath, oddPath, size);
     }
+    public static void createNumbersFile(int [] Array, String file) {
+        printToFile_x10(Array, file);
     }
-    public static void createOddNumbersFile() throws IOException {
-        Path path = Paths.get("Windows-SSD(C):\\Users\\38099\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kozhara\\hw9\\numbers\"))
-    try (BufferedWriter w = Files.newBufferedWriter(annotherPath)) {
-        for (int n = 0; n <= array.length - 1; n++) {
-            if (n % 10 == 0) {
-                w.write("\n" + array[n] + " ");
+    public static void createOddNumbersFile(String file, String oddFile, int size) {
+        Path path = Paths.get(file);
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String lineFull = "", line;
+            while((line = reader.readline()) != null) {
+                lineFull = String.join(" ", lineFull, line);
             }
-            else
-                w.write(array[n] + " ");
+            int[] oddArray = toNumArray(lineFull, size);
+            printToFile_x10(oddArray, oddFile);
+        } catch (Exception ex) {
+            System.out.println("Error with file read: " + ex);
         }
-        }
-    }catch(Exception e) {
-        e.printStackTrace();
     }
-    public static void main(String[] args) {
-        try {
-            createNumbersFile();
-            createOddNumbersFile();
-        }catch (IOException e) {
-            e.printStackTrace();
+    private static int[] toNumArray(String line, int size) {
+        String [] split = line.split(" ");
+        int[] intArr = new int[size];
+        int x = 0;
+        for (String s : split) {
+            if (!s.equals("")) {
+                intArr[x] = Integer.parseInt(s);
+                if (intArr[x] % 2 == 0) {
+                    intArr[x] = 0;
+                }
+                x++;
+            }
+        }
+        return intArr;
+    }
+    private static void printToFile_x10(int [] Array, String file) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(file))) {
+            for (int i = 0; i < Array.length; i++) {
+                writer.write(String.valueOf(Array[i]));
+                if ((i + 1) % 10 == 0) {
+                    writer.write("\n");
+                }else {
+                    writer.write(" ");
+                }
+            }
+        }catch (Exception ex) {
+            System.out.println("Error with file write: " + ex);
         }
     }
 }
